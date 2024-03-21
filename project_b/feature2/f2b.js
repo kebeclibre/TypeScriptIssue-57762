d62c01585110cab208d2e3e15465f1c1af93f281
Lucas Perais <lpe@odoo.com>
2024-03-21 08:45:58 +0100

    Step 3
    This one makes sense, but the two previous steps are very confusing.

--- project_b/feature2/f2b.js
+++ project_b/feature2/f2b.js
@@ -1,4 +1,9 @@
-// Now that the import in f2.js has been commented
-// Type MyComponent again
+// After the jsconfig.json includes the files
+// from project_a in its codebase (compilerOptions.includes)
+// MyComponent from feature1 is up for import again
 
-// This time the class from feature won't be proposed
+// Note that this highly makes sense
+// But the two preceding steps are very confusing
+import { MyComponent } from "@feature1/f1";
+
+class MyComponent2 extends MyComponent {}
